21749841e842d2ac4a3fa58ff0214bf661fb27e6
Mike Daley <mike@71squared.com>
2016-04-11 19:43:29 +0100

    Move particle code to a seperate file

---
+++ Particles.asm
@@ -0,0 +1,233 @@
+;****************************************************************************************************************
+; Generate three particles from the particle pool using the location passed in BC
+;****************************************************************************************************************
+genPrtcl
+                push    bc
+                call    fndInctvPrtcl
+                or      a                           ; Check if A is zero...
+                ret     z                           ; ...and return if it is 
+                pop     bc
+
+                inc     b                           ; Move to the center of the block
+                inc     b
+                inc     b
+                inc     b
+
+                push    bc                          ; Save B for use later now its been adjusted
+                push    bc
+
+                ld      a, 35                       ; Set lifespan of particle
+                ld      (hl), a                     ; save it
+                inc     l                           ; Move HL to...
+                inc     l                           ; ...the XVector
+                ld      a, (rndmNmbr1)
+                ld      (hl), a                     ; Load 0 into the XVector
+                inc     l                           ; Move HL to...
+                ld      (hl), 0x00                  ; Load 0 into the XVector
+                inc     l                           ; ...the Xpos
+                ld      (hl), 0                     ; Set the low byte to 0
+                inc     l                           ; Move to high byte
+                ld      (hl), b                     ; Set high byte to B
+                inc     l                           ; Move to the YVector
+                ld      a, (rndmNmbr3)
+                ld      (hl), a                     ; Load YVextor low byte
+                inc     l                           ; Move HL to...
+                ld      (hl), 0xfd                  ; Load YVextor high byte
+                inc     l                           ; ...the ypos
+                ld      (hl), 0                     ; Set the low byte to 0
+                inc     l                           ; Move to high byte
+                ld      (hl), c                     ; Set high byte to C
+
+                call    fndInctvPrtcl
+                pop     bc
+                or      a                           ; Check if A is zero...
+                ret     z                           ; ...and return if it is 
+
+                ld      a, 35                       ; Set lifespan of particle
+                ld      (hl), a                     ; save it
+                inc     l                           ; Move HL to...
+                inc     l                           ; ...the XVector
+                ld      a, (rndmNmbr2)
+                ld      (hl), a                     
+                inc     l                           
+                ld      (hl), 0xff                  
+                inc     l                         
+                ld      (hl), 0                   
+                inc     l                         
+                ld      (hl), b                   
+                inc     l                         
+                ld      a, (rndmNmbr3)
+                ld      (hl), a                   
+                inc     l                         
+                ld      (hl), 0xfd                
+                inc     l                         
+                ld      (hl), 0                   
+                inc     l                         
+                ld      (hl), c                   
+
+                call    fndInctvPrtcl
+                pop     bc
+                or      a                           ; Check if A is zero...
+                ret     z                           ; ...and return if it is 
+
+                ld      a, 35                       ; Set lifespan of particle
+                ld      (hl), a                     ; save it
+                inc     l                           ; Move HL to...
+                inc     l                           ; ...the XVector
+                ld      a, (rndmNmbr1)
+                ld      (hl), 0                     
+                inc     l                           
+                ld      (hl), 0                  
+                inc     l                         
+                ld      (hl), 0                   
+                inc     l                         
+                ld      (hl), b                   
+                inc     l                         
+                ld      a, (rndmNmbr3)
+                ld      (hl), a                   
+                inc     l                         
+                ld      (hl), 0xfc                
+                inc     l                         
+                ld      (hl), 0                   
+                inc     l                         
+                ld      (hl), c 
+
+                ret
+
+;****************************************************************************************************************
+; Find the address of a particle that is not currently active
+;
+; Entry Registers:
+;   NONE
+; Registers Used:
+;   A, B, D, E, H, L
+; Returned Registers:
+;   A = > 0 means a particle was found
+;   HL = Address of available score sprite
+;****************************************************************************************************************
+fndInctvPrtcl
+                ld      b, NUMPRTCLS                ; Load B with the total number of available particles 
+                ld      hl, objctPrtcls + 1         ; Load HL with the address of the first particles timer value
+_chkNxtPrtcl     
+                ld      a, (hl)                     ; Load A with the time value from the particle
+                or      a                           ; If its zero...
+                jr      z, _foundPrtcl              ; ...then its available and we can finish...
+                ld      de, PRTCLSZ                 ; ...otherwise we load DE with the size of a particle...
+                add     hl, de                      ; ... and increment HL to get to the next particle
+                djnz    _chkNxtPrtcl                ; Loop if necessary
+                xor     a                           ; Getting here means no available particles, so reset A
+                ret                                 ; Return
+_foundPrtcl
+                inc     a                           ; We found a particle so increment the timer to mark is used
+                ld      (hl), a
+                dec     l                           ; Move to the start of the particle struct
+                ret
+
+;****************************************************************************************************************
+; Updates active particles by adjusting their current position using their current vector. A particle is active
+; if it has a timer value > 0
+;
+; Entry Registers:
+;   NONE
+; Registers Used:
+;   A, B, D, E, H, L
+; Returned Registers:
+;   NONE
+;****************************************************************************************************************
+updtPrtcls
+                ld      b, NUMPRTCLS                ; Load B with the total number of particles available
+                ld      hl, objctPrtcls             ; Point HL at the particle objects object pool
+
+_nxtPrtcl 
+                ld      c, (hl)                     ; Save lifespan
+                inc     hl                          ; Move to timer
+                ld      a, (hl)                     ; Load timer
+                or      a                           ; Is it active > 0           
+                jr      nz, _updtPrtcl              ; Yes then update
+                ld      de, PRTCLSZ - 1             ; Move to next particle     
+                add     hl, de                      ; Increase HL
+                djnz    _nxtPrtcl                   ; Loop
+                ret
+
+_updtPrtcl
+                push    bc                          ; Save the counter in B
+
+                inc     a                           ; Increment timer
+                cp      c                           ; Compare with lifespan
+                jr      z, _rstPrtclTmr             ; If 0 then reset the timer
+                push    hl
+                ld      (hl), a                     ; Save new timer value
+                inc     l                          ; Move to the x vector address
+
+                ; Update X Position with XVector
+                ld      c, (hl)                     ; Load the low byte of the xVector into C
+                inc     l                           ; Move to the hight byte
+                ld      b, (hl)                     ; Load the hight byte of the xVector into B
+                inc     l
+                ld      e, (hl)                     ; Load low byte of xpos into E
+                inc     l                           ; Move to the high byte
+                ld      d, (hl)                     ; Load the high byte of xpos into D
+                ex      de, hl                      ; Exchange DE and HL 
+                add     hl, bc                      ; Add the xvector to the xpos
+                ex      de, hl                      ; Exchange DE and HL again to get the particle address back into HL
+
+                ld      a, d
+                cp      SCRNLFT
+                jr      nc, _sveXPos
+                ld      d, 8
+_sveXPos
+
+                ld      (hl), d                     ; Save high byte of xpos
+                dec     l                           ; Move to the low byte
+                ld      (hl), e                     ; Save the low byte of xpos
+                inc     l                           ; Move to the YVector
+                inc     l                           ; ...which is a word away    
+
+                ; Apply gravity to YVector
+                ld      c, (hl)                     ; Load the low byte of the xVector into C
+                inc     l                           ; Move to the hight byte
+                ld      b, (hl)                     ; Load the hight byte of the xVector into B
+                ld      a, (grvty)
+                ld      e, a
+                ld      a, (grvty + 1)
+                ld      d, a
+                ex      de, hl  
+                add     hl, bc
+                ex      de, hl
+                
+                ; Update Y Position using YVector
+                ld      c, e
+                ld      b, d
+                dec     l
+                ld      (hl), e
+                inc     l
+                ld      (hl), d
+                inc     l
+                ld      e, (hl)                     ; Load low byte of xpos into E
+                inc     l                           ; Move to the high byte
+                ld      d, (hl)                     ; Load the high byte of xpos into D
+                ex      de, hl                      ; Exchange DE and HL 
+                add     hl, bc                      ; Add the xvector to the xpos
+                ex      de, hl                      ; Exchange DE and HL again to get the particle address back into HL
+                ld      (hl), d                     ; Save high byte of xpos
+                dec     l                           ; Move to the low byte
+                ld      (hl), e                     ; Save the low byte of xpos
+                inc     l                           ; Move to the YVector
+                inc     l                           ; ...which is a word away 
+
+                ; Move to the next particle
+                pop     hl                          ; Resotore HL before we started moving around
+                pop     bc                          ; Restore our particle counter in B
+                ld      de, PRTCLSZ - 1             ; Load DE with the size of a particle struct - 1
+                add     hl, de                      ; Move HL to the next particle address
+                djnz    _nxtPrtcl                   ; Loop
+
+
+_rstPrtclTmr    ; Reset the timer for the current particle as its not dead
+                pop     bc                          ; Restore the particle counter in B
+                xor     a                           ; Clear A
+                ld      (hl), a                     ; Save A to the timer basically resetting it
+                ld      de, PRTCLSZ - 1             ; Load DE with the size of a particle struct - 1
+                add     hl, de                      ; Move HL to the next particle address
+                djnz    _nxtPrtcl                   ; Loop
+                ret
