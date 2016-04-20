813e6fb307f34d8db4b3928334ad05ca5ee00df1
Mike Daley <mike@daley-uk.com>
2016-04-20 11:53:52 +0100

    Updated location of emulators to run after the build script is run

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 pasmo --name paddle --tapbas -1 Paddle.asm Paddle.tap > debug.txt
 retval=$?
 if [ $retval -eq 0 ]; then
-    /Applications/Fuse\ for\ Mac\ OS\ X/Fuse.app/Contents/MacOS/Fuse Paddle.tap
+    /Applications/Spectrum\ Emulators/Fuse.app/Contents/MacOS/Fuse Paddle.tap
 fi
