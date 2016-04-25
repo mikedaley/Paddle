d7d58a3f2dd83d250fb66d5e2a730d07f6ae0bc2
Mike Daley <mike@71squared.com>
2016-04-25 14:56:07 +0100

    New level data structure and loading routine

--- Levels.asm
+++ Levels.asm
@@ -5,20 +5,9 @@
 ;*******************************************************************************************
 ; Level Table Constants
 ;*******************************************************************************************
-ROW_CLR_BYTES       equ     30
 
-LVL_CLR_ROW_1       equ     0
-LVL_CLR_ROW_2       equ     ROW_CLR_BYTES
-LVL_CLR_ROW_3       equ     ROW_CLR_BYTES * 2
-LVL_CLR_ROW_4       equ     ROW_CLR_BYTES * 3
-LVL_CLR_ROW_5       equ     ROW_CLR_BYTES * 4
-LVL_CLR_ROW_6       equ     ROW_CLR_BYTES * 5
-LVL_CLR_ROW_7       equ     ROW_CLR_BYTES * 6
-
-LEVEL_ROWS          equ     210
-
-LEVEL_BAT_SPEEDS    equ     LEVEL_ROWS + 105
-LEVEL_TITLE         equ     LEVEL_ROWS + 105 + 4
+LEVEL_BAT_SPEEDS    equ     224
+LEVEL_TITLE         equ     228
 
 BLOCKS_PER_ROW      equ     11
 
@@ -27,11 +16,50 @@ NMBR_CLMNS          equ     14
 NMBR_RWS            equ     8
 
 STRT_X_POS          equ     2 * 8
-STRT_Y_POS          equ     7 * 8
+STRT_CHR_X_POS      equ     2
+STRT_Y_POS          equ     5 * 8
+STRT_CHR_Y_POS      equ     5
+
+BLCK_CLR            equ     0
+BLCK_HIT_CNT        equ     1
+
+;****************************************************************************************************************
+; Variables on a byte boundary
+;****************************************************************************************************************
+                org     ($ + 255) & $ff00
+
+crrntRw         db      0
+crrntCl         db      0
+
+blckPosYX
+blckY           db      0
+blckX           db      0
+
+blckChrYX       
+blckChrX        db      0
+blckChrY        db      0
 
 ;****************************************************************************************************************
 ; Load Level
 ; A = Level to load 
+
+                ; When calculating the index into the level data the following calculation should be used  i = (x - 16) / 16
+                ; LD A, (XPOS)
+                ; SUB 16
+                ; RRA
+                ; RRA
+                ; RRA
+                ; RRA
+                ; LD (INDEX), A
+                ; LD A, (YPOS)
+                ; SUB 56
+                ; RRA
+                ; RRA
+                ; RRA
+                ; LD B, (INDEX)
+                ; ADD A, B
+                ; LD (INDEX), A
+
 ;****************************************************************************************************************
 ldLvl 
                 ld      hl, SCRNBFFR                ; Point HL at the screen buffer
@@ -51,106 +79,99 @@ ldLvl
 
                 ; Copy the level row data into the temp level data structure. This will be used to check how many
                 ; hits a block needs to be destroyed.
-;                 ld      de, LEVEL_ROWS
-;                 add     hl, de
                 push    de                          ; Save the current level address
                 ld      de, lvlData
                 ld      bc, BYTS_PER_CLMN * NMBR_CLMNS * NMBR_RWS
                 ldir
                 pop     hl                          ; Put the current level address into HL
 
-                ; When calculating the index into the level data the following calculation should be used  i = (x - 16) / 16
-                ; LD A, (XPOS)
-                ; SUB 16
-                ; RRA
-                ; RRA
-                ; RRA
-                ; RRA
-                ; LD (INDEX), A
-                ; LD A, (YPOS)
-                ; SUB 56
-                ; RRA
-                ; RRA
-                ; RRA
-                ; LD B, (INDEX)
-                ; ADD A, B
-                ; LD (INDEX), A
+                ; Init the variables for drawing the levels blocks to screen
+                xor     a                           ; Clear A
+                ld      (crrntRw), a                ; Reset the current row variable
+                ld      (crrntCl), a                ; Reset the current column variable
+                ld      a, STRT_X_POS               
+                ld      (blckX), a                  ; Init the Block X pos
+                ld      a, STRT_Y_POS
+                ld      (blckY), a                  ; Init the Block Y pos
+                ld      a, STRT_CHR_X_POS
+                ld      (blckChrX), a               ; Init the Block X char pos
+                ld      a, STRT_CHR_Y_POS
+                ld      (blckChrY), a               ; Init the Block Y char pos
 
+                ld      ix, (crrntLvlAddr)
+                ld      b, NMBR_CLMNS
+_rwLp         
+                push    bc
+                ld      a, (ix + BLCK_HIT_CNT)
+                or      a
+                jr      nz, _drwBlck
+                ld      a, (blckChrX)
+                inc     a
+                ld      (blckChrX), a
+_nxtClmn
+                ld      a, (blckChrX)
+                inc     a
+                ld      (blckChrX), a
+                ld      a, (blckX)
+                add     a, 16
+                ld      (blckX), a
+                inc     ix
+                inc     ix
 
+                pop     bc
+                djnz    _rwLp
 
+_nxtRw
+                ld      a, STRT_CHR_X_POS
+                ld      (blckChrX), a
+                ld      a, STRT_X_POS
+                ld      (blckX), a
 
+                ld      a, (blckChrY)
+                inc     a
+                cp      STRT_CHR_Y_POS + NMBR_RWS
+                ret     z
 
+                ld      (blckChrY), a
+                ld      a, (blckY)
+                add     a, 8
+                ld      (blckY), a
 
+                ld      b, NMBR_CLMNS
+                jp      _rwLp
 
-                ; Load the block colours from the level data into the attribute buffer starting at the 4th row
-                ld      a, 7                        ; Number of rows to load
-                ld      de, ATTRSCRNADDR + (32 * 7) ; Load into the 4th row of attributes
-_lvlRwLp
-                ld      bc, ROW_CLR_BYTES           ; Set how many bytes to copy
-                ldir                                ; Perform the copy
-                inc     de                          ; Move DE to the start of the next row in the...
-                inc     de                          ; ...attribute buffer
-                dec     a                           ; Reduce the row count...
-                jp      nz, _lvlRwLp                ; ...and loop if there are more rows to copy
-
-                ; Draw the blocks based on the levels block lookup table
-_nxtBlckRw      xor     a                           ; Clear A
-                ld      (currntBlckCl), a
-                ld      (currntBlckRw), a
-                ld      (currntBlckX), a
-                ld      a, 8 * 7
-                ld      (currntBlckY), a
-
-_drwNxtBlck     ld      bc, (currntBlckY)
-                ld      a, (hl)
-                inc     hl
-                cp      0
-                jr      z, _skpBlck
-
+_drwBlck
                 ld      a, (lvlBlckCnt)
                 inc     a
                 ld      (lvlBlckCnt), a
-
-                push    hl
+                ld      de, SpriteBlockData         ; Point to the block sprite data 
+                ld      bc, (blckPosYX)             ; Grab the Y and X position of the block
                 push    bc
-                ld      de, SpriteBlockData
-                xor     a
+                xor     a                           ; Want to draw to the screen file
                 call    drwSprt
+                ld      de, SpriteBlockData         ; Point to the block sprite data 
                 pop     bc
-                ld      de, SpriteBlockData
                 ld      a, 1
-                call    drwSprt
-                pop     hl
-
-_skpBlck        ld      a, (currntBlckX)
-                add     a, 16
-                ld      (currntBlckX), a
-                ld      a, (currntBlckCl)
-                inc     a
-                ld      (currntBlckCl), a
-                cp      15
-                jr      nz, _drwNxtBlck
-
-                xor     a
-                ld      (currntBlckX), a
-                ld      a, (currntBlckY)
-                add     a, 8
-                ld      (currntBlckY), a
-                xor     a
-                ld      (currntBlckCl), a
-
-                ld      a, (currntBlckRw)
-                inc     a
-                ld      (currntBlckRw), a
-                cp      7
-                jr      nz, _drwNxtBlck
-
-                ret
+                call    drwSprt                
                 
+                ld      a, (ix + BLCK_CLR)
+                ld      de, (blckChrYX)
+                call    setChrctrAttr
+                
+                ld      a, (blckChrX)
+                inc     a
+                ld      (blckChrX), a
+
+                ld      a, (ix + BLCK_CLR)
+                ld      de, (blckChrYX)
+                call    setChrctrAttr
+
+                jp      _nxtClmn
+
 ;*******************************************************************************************
 ; Level lookup table
 ;*******************************************************************************************
-lvlLkup         dw      Level1, Level2
+lvlLkup         dw      Level1, Level1, Level1, Level1, Level1, Level1, Level1
 
 ;*******************************************************************************************
 ; Level 1
@@ -160,16 +181,16 @@ Level1
                 ; Colour, # of hits to destroy
                 ; Each column represents a block which is 16 pixels wide and 8 pixels deep
                 ; A column containing 0,0 means there is no block at that position    
-                ; 15 columns of two bytes * 8 rows = 240 bytes
+                ; 14 columns of two bytes * 8 rows = 224 bytes
 
-                db  0,0,  0,0,  0,0,  0,0,  0,0, 65,1, 65,1, 65,1, 65,1,  0,0,  0,0,  0,0,  0,0,  0,0
-                db  0,0,  0,0,  0,0,  0,0,  2,1,  2,1,  2,1,  2,1,  2,1,  2,1,  0,0,  0,0,  0,0,  0,0
-                db  0,0,  0,0,  0,0,  3,1,  3,1,  3,1,  3,1,  3,1,  3,1,  3,1,  3,1,  0,0,  0,0,  0,0
-                db  0,0,  0,0, 68,1,  4,1, 68,1,  4,1, 68,1,  4,1, 68,1,  4,1, 68,1, 68,1,  0,0,  0,0
-                db  0,0,  0,0,  0,0,  3,1,  3,1,  3,1,  3,1,  3,1,  3,1,  3,1,  3,1,  0,0,  0,0,  0,0
-                db  0,0,  0,0,  0,0,  0,0,  2,1,  2,1,  2,1,  2,1,  2,1,  2,1,  0,0,  0,0,  0,0,  0,0
-                db  0,0,  0,0,  0,0,  0,0,  0,0, 65,1, 65,1, 65,1, 65,1,  0,0,  0,0,  0,0,  0,0,  0,0
-                db  0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0,  0,0
+                db  1,1,  2,1,  3,1,  4,1,  6,1, 66,1, 65,1, 65,1, 66,1,  6,1,  4,1,  3,1,  2,1,  1,1
+                db  1,1,  2,1,  3,1,  4,1,  6,1, 65,1, 65,1, 65,1, 65,1,  6,1,  4,1,  3,1,  2,1,  1,1
+                db  1,1,  2,1,  3,1,  4,1,  6,1, 66,1, 65,1, 65,1, 66,1,  6,1,  4,1,  3,1,  2,1,  1,1
+                db  1,1,  2,0,  3,0,  4,1,  6,1, 65,1, 65,0, 65,0, 65,1,  6,1,  4,1,  3,0,  2,0,  1,1
+                db  1,1,  2,0,  3,0,  4,1,  6,1, 66,1, 65,0, 65,0, 66,1,  6,1,  4,1,  3,0,  2,0,  1,1
+                db  1,1,  2,1,  3,1,  4,1,  6,1, 65,1, 65,0, 65,0, 65,1,  6,1,  4,1,  3,1,  2,1,  1,1
+                db  1,1,  2,1,  3,1,  4,1,  6,1, 66,1, 65,1, 65,1, 66,1,  6,1,  4,1,  3,1,  2,1,  1,1
+                db  1,1,  2,1,  3,1,  4,1,  6,1, 65,1, 70,1, 70,1, 65,1,  6,1,  4,1,  3,1,  2,1,  1,1
 
 Level1BatSpeed      
                 db 1, 2, 2, 3
@@ -178,34 +199,4 @@ Level1Title
 Level1TitleClear    
                 db 12, 16, 5, 22, 15, 12, '       '
 
-
-;*******************************************************************************************
-; Level 2
-;*******************************************************************************************
-                    org     ($ + 255) & $ff00
-Level2
-                    ; Table used to define the colours to be used in the blocks area of the
-                    ; screen
-Level2Colour
-                    db  5,  5,  7,  7,  7,  7,  7,  7,  7,  7,  5,  5,  5,  5,  7,  7,  7,  7,  5,  5,  5,  5,  5,  5,  7,  7,  7,  7,  5,  5
-                    db  5,  5,  5,  5,  5,  5,  5,  5,  7,  7,  5,  5,  7,  7,  5,  5,  5,  5,  7,  7,  5,  5,  7,  7,  5,  5,  5,  5,  7,  7
-                    db  5,  5,  5,  5,  5,  5,  7,  7,  5,  5,  5,  5,  7,  7,  5,  5,  5,  5,  7,  7,  5,  5,  7,  7,  5,  5,  5,  5,  7,  7
-                    db  5,  5,  5,  5,  7,  7,  5,  5,  5,  5,  5,  5,  7,  7,  7,  7,  7,  7,  7,  7,  5,  5,  7,  7,  5,  5,  5,  5,  7,  7
-                    db  5,  5,  7,  7,  5,  5,  5,  5,  5,  5,  5,  5,  7,  7,  5,  5,  5,  5,  7,  7,  5,  5,  7,  7,  5,  5,  5,  5,  7,  7
-                    db  5,  5,  7,  7,  5,  5,  5,  5,  5,  5,  5,  5,  7,  7,  5,  5,  5,  5,  7,  7,  5,  5,  7,  7,  5,  5,  5,  5,  7,  7
-                    db  5,  5,  7,  7,  7,  7,  7,  7,  7,  7,  5,  5,  5,  5,  7,  7,  7,  7,  5,  5,  5,  5,  5,  5,  7,  7,  7,  7,  5,  5
-
-                    ; Table used to define where blocks are positioned on screen
-Level2Rows
-                    db 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0
-                    db 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1
-                    db 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1
-                    db 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1
-                    db 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1
-                    db 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1
-                    db 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0
-
-Level2BatSpeed      db 1, 2, 2, 3
-Level2Title         db 12, 16, 5, 22, 15, 12, 'LEVEL 2'
-Level2TitleClear    db 12, 16, 5, 22, 15, 12, '       '
 
