69ca07df4d96682e7fa8b027a10cdc51cae11105
Mike Daley <mike@71squared.com>
2016-06-15 16:01:40 +0100

    Added debug output file

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ echo -ne "Assembling file:" $1 "\n"
 tapFile=$(basename $1 .asm).tap
 echo -ne "Generating TAP file:" $tapFile "\n"
 echo -ne "************************************************************************\n\n"
-/usr/local/bin/pasmo --tapbas -1 "$1" "$tapFile"
+/usr/local/bin/pasmo --tapbas -1 "$1" "$tapFile" >> debug.txt
